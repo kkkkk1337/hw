741855cae4c475d111b5e365afed4446957ff6ce
kkkkk <51333302+kkkkk1337@users.noreply.github.com>
2019-06-04 22:29:42 +0600

    Add files via upload

---
+++ scratch_1.py
@@ -0,0 +1,5 @@
+def text(a,b):
+    print ("pozdravlyayu, "+(a)+" vam "+(b)+" let i vi prohodite uglublenniy kurs python")
+a = str(input())
+b = str(input())
+text(a,b)
---
+++ scratch_2.py
@@ -0,0 +1,8 @@
+def porabola(x,k):
+    return k/x
+print("VVEDITE KOEFICENT ")
+k = int(input())
+print("VVEDITE ZNACHENIE X--> ")
+x = int(input())
+print("Y == ")
+print(porabola(x,k))
